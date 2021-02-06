eb824c87c8ea527edf471821b0fb3dec3fd9b772
razvan neda <neda_razvan@yahoo.com>
2021-02-06 16:53:34 +0000

    basic back-end done

--- app/models/suit.rb
+++ app/models/suit.rb
@@ -1,2 +1,9 @@
 class Suit < ApplicationRecord
+ belongs_to :user 
+
+ validates :size, presence: true 
+ validates :colour, presence: true
+ validates :style, presence: true
+ validates :occasion, presence: true, inclusion: {in: %w(interview funeral wedding party date)}
+ validates :price, presence: true
 end
