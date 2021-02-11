5350629b257b0fd75b5ec5267589f67f9bd676dd
razvan neda <neda_razvan@yahoo.com>
2021-02-11 19:33:53 +0000

    Added name to Suits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_195340) do
+ActiveRecord::Schema.define(version: 2021_02_11_193215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_02_09_195340) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "brand"
+    t.string "name"
     t.index ["user_id"], name: "index_suits_on_user_id"
   end
 
