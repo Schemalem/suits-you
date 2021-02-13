484784bd61c253175a1dbbfb1070022933fa4d3c
Nicole Schembri <nschembri93@gmail.com>
2021-02-13 15:32:50 +0000

    Fixing seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 puts "Cleaning database..."
 Suit.destroy_all
+User.destroy_all
 
 puts "Creating Suits..."
-10.times do 
+10.times do
   user = User.create(
       email: Faker::Internet.email,
       password: "hello12345"
@@ -12,11 +13,11 @@ end
 100.times do
   suit = Suit.create(
     user_id: User.all.sample.id,
-    name: Faker::Superhero.name, 
+    name: Faker::Superhero.name,
     size: rand(32..50),
-    colour: Faker::Color.color_name, 
+    colour: Faker::Color.color_name,
     style: ["Formal", "Posh", "Hipster", "Informal"].sample,
-    occasion:["interview", "funeral", "wedding", "party", "date"].sample,
+    occasion:["Interview", "Funeral", "Wedding", "Party", "Date"].sample,
     price: Faker::Commerce.price(range: 50..100.0, as_string: true),
     brand:["Hugo Boss", "Armani", "Zara", "H&M", ].sample,
     )
